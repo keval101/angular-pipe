fc0ccae99ab3bb3e008d227c50717d8956326faa
keval101 <kevalvadhiya101@gmail.com>
2021-07-20 19:27:09 +0530

    add pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pipe';
+
+  AppStatus = new Promise((resolve,reject) =>{
+    setInterval( () => {
+      resolve('Stable');
+    } ,2000)
+  }
+  )
+  servers = [
+    {
+      instanceType : "medium",
+      name : 'Production',
+      status : 'stable',
+      started : new Date()
+    },
+    {
+      instanceType : "large",
+      name : 'User Database',
+      status : 'stable',
+      started : new Date()
+    },
+    {
+      instanceType : "small",
+      name : 'Development Server',
+      status : 'offline',
+      started : new Date()
+    },
+    {
+      instanceType : "small",
+      name : 'Testing Server',
+      status : 'stable',
+      started : new Date()
+    }
+  ];
+
+  filterStatus='';
+
+  getStatusClasses(server: {instanceType:string, name:string, status:string, started:Date}){
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    }
+  }
+
+  onAddServer(){
+    this.servers.push({
+      instanceType : 'small',
+      name:'New Added Server',
+      status: 'stable',
+      started : new Date()
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomPipePipe } from './custom-pipe.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomPipePipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
